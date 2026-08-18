22ed5c763c7198807a34940cd9a491f10de4c16f
Zhao Yumeng <AnnieZhao56@icloud.com>
2026-08-19 00:23:29 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Yuki.java
@@ -0,0 +1,18 @@
+public class Yuki {
+    public static void main(String[] args) {
+        String horizontal_line = "____________________________________________________________";
+        String banner = "__   __     _    _ \n"
+                + "\\ \\ / /   _| | _(_)\n"
+                + " \\ V / | | | |/ / |\n"
+                + "  | || |_| |   <| |\n"
+                + "  |_| \\__,_|_|\\_\\_|\n";
+
+        System.out.println(horizontal_line);
+        System.out.println(banner);
+        System.out.println("...Hello. This is Yuki.");
+        System.out.println("What do you need?");
+        System.out.println(horizontal_line);
+        System.out.println("...Goodbye.");
+        System.out.println(horizontal_line);
+    }
+}
