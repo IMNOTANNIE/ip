14444fc24b69027ab9c85fc8517d151b4a6b8b1d
Zhao Yumeng <AnnieZhao56@icloud.com>
2026-08-19 10:44:40 +0800

    Level-1: Echo

--- src/main/java/Yuki.java
+++ src/main/java/Yuki.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
+
 public class Yuki {
     public static void main(String[] args) {
-        String horizontal_line = "____________________________________________________________";
+        String horizontalLine = "❄──────❄──────❄──────❄──────❄──────❄──────❄";
         String banner = "__   __     _    _ \n"
                 + "\\ \\ / /   _| | _(_)\n"
                 + " \\ V / | | | |/ / |\n"
                 + "  | || |_| |   <| |\n"
                 + "  |_| \\__,_|_|\\_\\_|\n";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(horizontal_line);
+        // Greet the user
+        System.out.println(horizontalLine);
         System.out.println(banner);
         System.out.println("...Hello. This is Yuki.");
         System.out.println("What do you need?");
-        System.out.println(horizontal_line);
-        System.out.println("...Goodbye.");
-        System.out.println(horizontal_line);
+        System.out.println(horizontalLine);
+
+        while (true) {
+            // Let the user enter a command and record the command
+            String command = sc.nextLine();
+            // Exit when the user types the command bye
+            if (command.equals("bye")) {
+                System.out.println(horizontalLine);
+                System.out.println("...Goodbye.");
+                System.out.println(horizontalLine);
+                break;
+            }
+            // Echo the command
+            System.out.println(horizontalLine);
+            System.out.println(command);
+            System.out.println(horizontalLine);
+        }
     }
 }
